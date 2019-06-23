c974aa9444bf98ea8d0e577480f8fef5fe2ab0d0
Alex McGregor <mcgregoralex3@gmail.com>
2019-06-23 01:09:17 -0700

    Able to get dataframes of the last 20 years of statistics

--- Scraper.py
+++ Scraper.py
@@ -3,14 +3,15 @@ from urllib.request import urlopen
 import pandas as pd
 
 # From a tutorial online (To learn, then going to work through it again by myself)
-year = 2019
-url = "https://www.basketball-reference.com/leagues/NBA_2019_per_game.html"
-html = urlopen(url)
-soup = BeautifulSoup(html, features="html.parser")
-headers = [th.getText for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-headers = headers[1:]
-print(headers)
-rows = soup.findAll('tr')[1:]
-player_stats = [[td.getText for td in rows[i].findAll('td')]for i in range(len(rows))]
-stats = pd.DataFrame(player_stats, columns=headers)
-print(stats.head(10))
+stats_list = []
+for i in range(2000, 2019):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(i) + "_per_game.html"
+    html = urlopen(url)
+    soup = BeautifulSoup(html)
+    headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+    headers = headers[1:]
+    rows = soup.findAll('tr')[1:]
+    player_data = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
+    stats = pd.DataFrame(player_data, columns=headers)
+    stats_list.append(stats)
+
